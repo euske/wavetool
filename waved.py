ee5985f86ef6ea8a2e077b8111287e3485b4c394
Yusuke Shinyama <yusuke@shinyama.jp>
2014-11-09 22:33:01 +0900

    Code cleanup: use Cursor object.

--- waved.py
+++ waved.py
@@ -2,50 +2,89 @@
 import sys
 import os.path
 import pygame
-import array
 from wavestream import WaveReader, WaveWriter, PygameWavePlayer
 
 class WavEdError(Exception): pass
 class WavNoFileError(Exception): pass
 
+def bound(x,y,z): return max(x, min(y, z))
+
+class Cursor(object):
+
+    def __init__(self, maxlength):
+        self.maxlength = maxlength
+        self.start = 0
+        self.end = 0
+        self.length = None
+        assert ((self.end is not None and self.length is None) or
+                (self.end is None and self.length is not None))
+        return
+
+    def __repr__(self):
+        if self.end is not None:
+            return 'start %d end %d' % (self.start, self.end)
+        else:
+            return 'start %d length %d' % (self.start, self.length)
+
+    def get_length(self):
+        if self.end is not None:
+            return (self.end - self.start)
+        else:
+            return self.length
+
+    def set_start(self, start):
+        self.start = bound(0, start, self.maxlength)
+        if self.length is not None:
+            self.length = bound(0, self.length, self.maxlength-self.start)
+        return
+
+    def set_end(self, end):
+        self.end = bound(0, end, self.maxlength)
+        self.length = None
+        return
+
+    def set_length(self, length):
+        self.end = None
+        self.length = bound(0, length, self.maxlength-self.start)
+        return
+    
 class WavEd(object):
 
     def __init__(self):
         self._wav = None
+        self._cur = None
         return
 
     def close(self):
         if self._wav is not None:
             self._wav.close()
             self._wav = None
+            self._cur = None
         return
 
     def read(self, path):
         self.close()
         self._wav = WaveReader(path)
-        self._start = 0
-        self._end = 0
-        self._length = self._wav.framerate
-        self._adjust()
+        self._cur = Cursor(self._wav.nframes)
+        self._cur.set_length(self._wav.framerate)
         print ('Read: rate=%d, frames=%d, duration=%.3f' %
                (self._wav.framerate, self._wav.nframes,
                 self._wav.nframes/float(self._wav.framerate)))
         return
 
     def write(self, path, force=False):
-        if self._wav is None: raise WavNoFileError
+        if self._cur is None: raise WavNoFileError
         if not force and os.path.exists(path):
             raise WavEdError('File exists: %r' % path)
-        if self._start < self._end:
-            nframes = self._end - self._start
-        else:
-            nframes = self._length
+        nframes = self._cur.get_length()
+        if nframes == 0:
+            raise WavEdError('Empty range')
         fp = open(path, 'wb')
         writer = WaveWriter(fp,
                             nchannels=self._wav.nchannels,
                             sampwidth=self._wav.sampwidth,
                             framerate=self._wav.framerate)
-        self._wav.seek(self._start)
+        self._wav.seek(self._cur.start)
         (_,data) = self._wav.readraw(nframes)
         writer.writeraw(data)
         writer.close()
@@ -56,26 +95,20 @@ class WavEd(object):
         return
 
     def play(self):
-        if self._wav is None: raise WavNoFileError
-        if self._start < self._end:
-            nframes = self._end - self._start
-        else:
-            nframes = self._length
+        if self._cur is None: raise WavNoFileError
+        nframes = self._cur.get_length()
         player = PygameWavePlayer(nchannels=self._wav.nchannels,
                                   sampwidth=self._wav.sampwidth,
                                   framerate=self._wav.framerate)
-        self._wav.seek(self._start)
+        self._wav.seek(self._cur.start)
         (_,data) = self._wav.readraw(nframes)
         player.writeraw(data)
         player.close()
         return
 
     def status(self):
-        if self._wav is None: raise WavNoFileError
-        elif self._start < self._end:
-            print 'start %d end %d' % (self._start, self._end)
-        else:
-            print 'start %d length %d' % (self._start, self._length)
+        if self._cur is None: raise WavNoFileError
+        print self._cur
         return
 
     def _getv(self, v):
@@ -85,12 +118,6 @@ class WavEd(object):
         else:
             return int(v)
 
-    def _adjust(self):
-        if self._wav is None: raise WavNoFileError
-        self._length = (min(self._start+self._length, self._wav.nframes)-
-                        self._start)
-        return
-        
     def run(self):
         self.command('p')
         while 1:
@@ -107,8 +134,7 @@ class WavEd(object):
     def command(self, s):
         try:
             if s[0].isdigit():
-                self._start = self._getv(s)
-                self._adjust()
+                self._cur.set_start(self._getv(s))
                 self.status()
                 self.play()
                 return True
@@ -126,19 +152,15 @@ class WavEd(object):
                 self.status()
                 self.play()
             elif c == 's':
-                self._start = self._getv(v)
-                self._adjust()
+                self._cur.set_start(self._getv(v))
                 self.status()
                 self.play()
             elif c == 'e':
-                self._end = self._getv(v)
-                self._adjust()
+                self._cur.set_end(self._getv(v))
                 self.status()
                 self.play()
             elif c == 'l':
-                self._length = self._getv(v)
-                self._end = -1
-                self._adjust()
+                self._cur.set_length(self._getv(v))
                 self.status()
                 self.play()
             else:
