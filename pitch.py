134e6a1637bc714ecf57b97ddf7b7544f3b43855
Yusuke Shinyama <yusuke@shinyama.jp>
2014-12-17 09:55:25 +0900

    Added range spec. support.

--- pitch.py
+++ pitch.py
@@ -105,6 +105,17 @@ def main(argv):
     def usage():
         print 'usage: %s [-d] [-M|-F] [-n pitchmin] [-m pitchmax] [-T threshold_sim] [-S threshold_mag] wav ...' % argv[0]
         return 100
+    def parse_range(x):
+        (b,_,e) = x.partition('-')
+        try:
+            b = int(b)
+        except ValueError:
+            b = 0
+        try:
+            e = int(e)
+        except ValueError:
+            e = 0
+        return (b,e)
     try:
         (opts, args) = getopt.getopt(argv[1:], 'dMFn:m:T:S:')
     except getopt.GetoptError:
@@ -125,8 +136,12 @@ def main(argv):
         elif k == '-S': threshold_mag = float(v)
     detector = None
     smoother = None
-    for path in args:
-        print '#', path
+    for arg1 in args:
+        (path,_,ranges) = arg1.partition(':')
+        ranges = [ parse_range(x) for x in ranges.split(',') if x ]
+        if not ranges:
+            ranges.append((0,0))
+        print '#', path, ranges
         src = WaveReader(path)
         if src.nchannels != 1: raise ValueError('invalid number of channels')
         if src.sampwidth != 2: raise ValueError('invalid sampling width')
@@ -138,24 +153,30 @@ def main(argv):
                                      windowsize=2*src.framerate/pitchmin,
                                      threshold_sim=threshold_sim,
                                      threshold_mag=threshold_mag)
-        i = 0
-        skip = True
-        while 1:
-            (nframes,buf) = src.readraw(bufsize)
-            if not nframes: break
-            seq = detector.feed(buf, nframes)
-            for (n,pitches,data) in seq:
-                if debug:
-                    print ('# %d: %r' % (i, pitches))
-                for (n,spitch) in smoother.feed(n, pitches):
-                    if spitch:
-                        print i, ' '.join( repr(pitch) for (sim,pitch) in spitch )
-                        skip = False
-                    else:
-                        if not skip:
-                            print
-                            skip = True
-                    i += n
+        for (b,e) in ranges:
+            if e == 0:
+                e = src.nframes
+            src.seek(b)
+            length = e-b
+            i = b
+            skip = True
+            while length:
+                (nframes,buf) = src.readraw(min(bufsize, length))
+                if not nframes: break
+                length -= nframes
+                seq = detector.feed(buf, nframes)
+                for (n,pitches,data) in seq:
+                    if debug:
+                        print ('# %d: %r' % (i, pitches))
+                    for (n,spitch) in smoother.feed(n, pitches):
+                        if spitch:
+                            print i, ' '.join( repr(pitch) for (sim,pitch) in spitch )
+                            skip = False
+                        else:
+                            if not skip:
+                                print
+                                skip = True
+                        i += n
         src.close()
     return
 
